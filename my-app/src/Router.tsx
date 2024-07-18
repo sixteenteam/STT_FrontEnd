f2f092b9a97076fe908905ef6b31d7c20dbdf647
juwon <juwon2298@naver.com>
2024-07-18 09:45:55 +0900

    배포 전 커밋

--- my-app/src/Router.tsx
+++ my-app/src/Router.tsx
@@ -10,10 +10,10 @@ import QuizHistory from './pages/QuizHistory';
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/scul/users/login" element={<SocialKakao />} />
+      {/* <Route path="/" element={<LandingPage />} /> */}
+      {/* <Route path="/signup" element={<SignUp />} /> */}
+      {/* <Route path="/login" element={<Login />} /> */}
+      {/* <Route path="/scul/users/login" element={<SocialKakao />} /> */}
       <Route path="/myPage/:token" element={<MyPage />} />
       <Route path="/quiz/:questionNumber" element={<Quiz />} />
       <Route path="/quiz/history" element={<QuizHistory />} />
