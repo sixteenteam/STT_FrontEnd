5e74949745ebaea1a11b9e390dbe10f28f6d42ac
juwon <juwon2298@naver.com>
2024-07-18 09:48:40 +0900

    라우터 수정

--- my-app/src/Router.tsx
+++ my-app/src/Router.tsx
@@ -1,8 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/Landing';
-import SignUp from './pages/Signup';
-import Login from './pages/Login';
-import SocialKakao from './SocialKaKao';
 import MyPage from './pages/myPage';
 import Quiz from './pages/Quiz';
 import QuizHistory from './pages/QuizHistory';
@@ -10,10 +6,6 @@ import QuizHistory from './pages/QuizHistory';
 const Router = () => {
   return (
     <Routes>
-      {/* <Route path="/" element={<LandingPage />} /> */}
-      {/* <Route path="/signup" element={<SignUp />} /> */}
-      {/* <Route path="/login" element={<Login />} /> */}
-      {/* <Route path="/scul/users/login" element={<SocialKakao />} /> */}
       <Route path="/myPage/:token" element={<MyPage />} />
       <Route path="/quiz/:questionNumber" element={<Quiz />} />
       <Route path="/quiz/history" element={<QuizHistory />} />
